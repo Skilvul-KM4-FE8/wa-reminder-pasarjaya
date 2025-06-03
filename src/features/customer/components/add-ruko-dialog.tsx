a65b35e49570e0f305f991e9b0e8967f03a128c6
Muhamad Alfito Santosa <sanskun02pg@gmail.com>
2025-06-03 16:17:50 +0700

    push dev branch

--- src/features/customer/components/add-ruko-dialog.tsx
+++ src/features/customer/components/add-ruko-dialog.tsx
@@ -1,19 +1,11 @@
-"use client"
+"use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { useAddRuko } from "../hooks/use-add-dialog"
-import { Button } from "@/components/ui/button"
-
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { useAddRuko } from "../hooks/use-add-dialog";
+import { Button } from "@/components/ui/button";
 
 export const AddRukoDialog = () => {
-  const { isOpen, onOpen, onClose } = useAddRuko()
+  const { isOpen, onOpen, onClose } = useAddRuko();
 
   return (
     <Dialog open={isOpen} onOpenChange={onOpen}>
@@ -22,8 +14,7 @@ export const AddRukoDialog = () => {
         <DialogHeader>
           <DialogTitle>Are you absolutely sure?</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
+            This action cannot be undone. This will permanently delete your account and remove your data from our servers.
             <Button variant="destructive" onClick={onClose} className="mt-4">
               Close
             </Button>
@@ -31,5 +22,5 @@ export const AddRukoDialog = () => {
         </DialogHeader>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
