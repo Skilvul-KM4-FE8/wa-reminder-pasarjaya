d19a276ab80a0f5d9e25d7cbeb8c9ffa6f6d2abb
Muhamad Alfito Santosa <sanskun02pg@gmail.com>
2025-06-07 16:53:07 +0700

    push page wa reminders and overview

--- src/app/reminders/page.tsx
+++ src/app/reminders/page.tsx
@@ -1,9 +1,14 @@
+import { Header } from "../components/header";
+
 export default function BroadcastPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Broadcast Page</h1>
-      <p className="text-lg">This is the broadcast page.</p>
-      {/* Add your broadcast functionality here */}
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold mb-4">Reminders Log Page</h1>
+        <p className="text-lg">This is the broadcast page.</p>
+        {/* Add your broadcast functionality here */}
+      </div>
+    </>
   );
 }
--- src/app/whatsapp/page.tsx
+++ src/app/whatsapp/page.tsx
@@ -1,9 +1,14 @@
+import { Header } from "../components/header";
+
 export default function WhatsappPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">WhatsApp Page</h1>
-      <p className="text-lg">This is the WhatsApp page.</p>
-      {/* Add your WhatsApp functionality here */}
-    </div>
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold mb-4">WhatsApp Page</h1>
+        <p className="text-lg">This is the WhatsApp page.</p>
+        {/* Add your WhatsApp functionality here */}
+      </div>
+    </>
   );
 }
